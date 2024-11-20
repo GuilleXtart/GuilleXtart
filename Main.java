4b078814af335bf74fdb9480621387b502b1350e
GuilleXtart <guillermo.sanchez@estudiante.xtart.com>
2024-11-20 13:03:11 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+Scanner sc = new Scanner(System.in);
 
-        System.out.println("Introduce un número: ");
+System.out.println("INTRODUCE 3 NÚMEROS.");
+
+int n1= 0;
+int n2 = 0;
+int n3 = 0;
+
+for(int i=1;i<=3;i++){
+    System.out.print("Introduce el número " +i+ ": ");
+    if(i==1){
+        n1 = sc.nextInt();
+    } else if (i==2) {
+        n2 = sc.nextInt();
+    } else{
+        n3 = sc.nextInt();
+    }
+}
+/*
+System.out.print("Introduce el primer número: ");
+int n1 = sc.nextInt();
+
+System.out.print("Introduce el segundo número: ");
+int n2 = sc.nextInt();
+
+System.out.print("Introduce el tercer número: ");
+int n3 = sc.nextInt();
+*/
+if (n1 + n2 == n3)
+    System.out.println("La suma de los 2 primeros números corresponden al 3er número.");
+else
+    System.out.println("La suma de los 2 primeros números no corresponden al 3er número.");
 
 
-        }
 
     }
-
-
+}
